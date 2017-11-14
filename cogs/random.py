87d82b3c89c1611852cd07c70bf418198857a78d
trainboy2019 <ikerolader@gmail.com>
2017-11-14 18:39:23 -0500

    More tests

--- cogs/random.py
+++ cogs/random.py
@@ -233,8 +233,9 @@ class RandomStuff(Base):
 
 
     @commands.command(pass_context = True)
-    async def wantedTest(self, ctx, member : discord.Member, text : str = "5000"):
+    async def wantedTest(self, ctx, member : discord.Member, text : str = None):
         """?"""
+        await self.bot.say(text)
         await self.bot.send_typing(ctx.message.channel)
         glitcher=False
         reasonBool=False
