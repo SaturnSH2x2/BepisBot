7ea6d86409be5cb45e18da22411ba1fddf30c8d2
BEPISMAN2 <helpithinkihavehermes@gmail.com>
2017-11-15 01:36:37 +0000

    added warning system

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,5 +1,6 @@
 import asyncio
 import discord
+import os
 from discord.ext import commands
 
 import util
@@ -9,6 +10,12 @@ class Moderator(base.Base):
 	def __init__(self, bot, config):
 		self.mod_roles = config["moderator-roles"]
 		super().__init__(bot)
+
+		# make sure this directory exists
+		try:
+			os.mkdir("warns")
+		except FileExistsError:
+			pass
 
 	@commands.command(pass_context = True)
 	async def kick(self, ctx, member : discord.Member):
@@ -21,6 +28,49 @@ class Moderator(base.Base):
 				await self.bot.say("{} has been kicked from the server.".format(member.name))
 			except:
 				await self.bot.say("Could not kick member.  This might be a permissions issue.")
+
+	@commands.command(pass_context = True)
+	async def unwarn(self, ctx, member : discord.Member, amount : int = 1):
+		perms = await util.check_perms(self, ctx)
+		if not perms:
+			return
+
+		serverWarnList = util.load_js(os.path.join("warns", "{}.json".format(ctx.message.server.id)))
+		if member.id not in serverWarnList or serverWarnList[member.id] <= 0:
+			await self.bot.say("{} does not currently have any warns".format(member.name))
+			return
+
+		if amount > 5:
+			amount = 5
+
+		serverWarnList[member.id] -= amount
+		await self.bot.say("<@!{}>, you have had {} warns removed.  {} warnings remain.".format(member.id, amount, 5 - serverWarnList[member.id]))
+		util.save_js(os.path.join("warns", "{}.json".format(ctx.message.server.id)), serverWarnList)
+
+	@commands.command(pass_context = True)
+	async def warn(self, ctx, member : discord.Member):
+		perms = await util.check_perms(self, ctx)
+		if not perms:
+			return
+
+		serverWarnList = util.load_js(os.path.join("warns", "{}.json".format(ctx.message.server.id)))
+
+		if member.id not in serverWarnList:
+			serverWarnList[member.id] = 0
+		serverWarnList[member.id] += 1
+
+		if 5 - serverWarnList[member.id] > 1:
+			await self.bot.say("<@!{}>, you have been given a warning.  You have {} warnings left.".format(member.id, 5 - serverWarnList[member.id]))
+		elif serverWarnList[member.id] >= 5:
+			pass
+		else:
+			await self.bot.say("<@!{}>, this is your final warning.  One more warn and you will be banned.".format(member.id))
+		util.save_js(os.path.join("warns", "{}.json".format(ctx.message.server.id)), serverWarnList)
+
+		if serverWarnList[member.id] >= 5:
+			await self.bot.ban(member)
+			await self.bot.say("<@!{}> has been banned from the server for getting 5 warnings.".format(member.id))
+		
 
 def setup(bot):
 	config = util.load_js("config.json")
