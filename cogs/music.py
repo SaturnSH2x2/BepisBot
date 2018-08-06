14fbcbacb413681346815c31c5fdf3d3f0c0ce45
Bepis <helpithinkihavehermes@gmail.com>
2018-08-06 01:49:16 -0400

    music cog now works on a per-server basis

--- cogs/music.py
+++ cogs/music.py
@@ -30,8 +30,6 @@ def downloadVideo(ytLink : str, filename : str, q : mp.Queue):
         q.put(info)
         ydl.download([ytLink])
 
-# TODO: this code won't work on a per-server basis. Keep track of what VC
-# the bot is in on by server (you may need to use dictionaries for this)
 class Music(Base):
     "Commands for playing music and audio from YouTube video links."
     def __init__(self, bot):
@@ -40,8 +38,8 @@ class Music(Base):
         else:
             discord.opus.load_opus(find_library("opus"))
             
-        self.voice = None
-        self.musicList = []
+        self.voice = {}
+        self.musicList = {}
         super().__init__(bot)
         
     # helper function to test if Opus is loaded
@@ -56,10 +54,13 @@ class Music(Base):
     # helper function to avoid repeating code for checking certain
     # command conditions
     async def checkPermissions(self, ctx):
-        if self.voice == None:
-            await ctx.send("Not currently in a voice channel.")
+        if ctx.guild.id not in self.voice.keys() or \
+            self.voice[ctx.guild.id] == None or \
+            ctx.author.voice == None:
+            await ctx.send("You're not connected to a voice channel.  " + 
+                "Do that first, then call this command.")
             return False
-        if self.voice.channel != ctx.author.voice.channel:
+        elif self.voice[ctx.guild.id].channel != ctx.author.voice.channel:
             await ctx.send("I need to be in the same voice channel as you " +
                             "to do that!")
             return False
@@ -74,18 +75,20 @@ class Music(Base):
             return
             
         # get the voice channel the user is currently in
-        vChannel = ctx.message.author.voice.channel
-        if vChannel == None:
-            await self.bot.say("You are not connected to a voice channel.  " + 
-                "Connect first, then call this command.")
+        try:
+            vChannel = ctx.message.author.voice.channel
+        except AttributeError:
+            await ctx.send("You're not connected to a voice channel.  " + 
+                "Do that first, then call this command.")
             return
             
         # connect to a voice channel. If bot is in another voice channel, switch
         # to the channel specified.
         try:
-            self.voice = await vChannel.connect()
+            self.voice[ctx.guild.id] = await vChannel.connect()
         except discord.ClientException:
-            await self.voice.move_to(vChannel)
+            await self.voice[ctx.guild.id].move_to(vChannel)
+        self.musicList[ctx.guild.id] = []
 
         await ctx.send("Ready!")
 
@@ -95,8 +98,11 @@ class Music(Base):
         if not await self.checkPermissions(ctx):
             return
 
-        await self.voice.disconnect()
+        await self.voice[ctx.guild.id].disconnect()
         await ctx.send("Left the voice channel.")
+
+        self.voice.pop(ctx.guild.id, None)
+        self.musicList.pop(ctx.guild.id, None)
         
     @commands.command()
     async def play(self, ctx, ytLink : str):
@@ -122,31 +128,32 @@ class Music(Base):
             await asyncio.sleep(0.75)
 
         vidInfo = vidInfoQueue.get()
-        self.musicList += [[filename, vidInfo["title"]]]
+        self.musicList[ctx.guild.id] += [[filename, vidInfo["title"]]]
     
-        if self.voice.is_playing():
+        if self.voice[ctx.guild.id].is_playing():
             await ctx.send("**%s** has been added to the queue (position in queue: %i)" % \
-                    (vidInfo["title"], len(self.musicList)))
+                    (vidInfo["title"], len(self.musicList[ctx.guild.id])))
             return
 
         # playback everything in the list
-        while len(self.musicList) > 0:
-            listEntry = self.musicList[0]
+        while len(self.musicList[ctx.guild.id]) > 0:
+            listEntry = self.musicList[ctx.guild.id][0]
             filename = listEntry[0]
             title = listEntry[1]
 
             ffas = discord.FFmpegPCMAudio(filename + ".opus")
-            self.voice.play(ffas)
+            self.voice[ctx.guild.id].play(ffas)
             await ctx.send("Now playing: **%s**" % (title))
 
             # wait for the audio playback to stop
-            while (self.voice.is_playing() or self.voice.is_paused()):
+            while (self.voice[ctx.guild.id].is_playing() or \
+                    self.voice[ctx.guild.id].is_paused()):
                 await asyncio.sleep(0.75)
 
             # the bot doesn't cache music files, so get rid of them
             # once we're done streaming the music
             os.remove(filename + ".opus")
-            self.musicList.remove(listEntry)
+            self.musicList[ctx.guild.id].remove(listEntry)
         
         await ctx.send("Playback stopped.")
 
@@ -156,9 +163,11 @@ class Music(Base):
         "List all items in the current playback queue."
         queueString = ""
 
-        queueString += "Now playing: **%s**\n\n" % (self.musicList[0][1])
-        for i in range(1, len(self.musicList)):
-            queueString += "%i.  **%s**\n" % (i + 1, self.musicList[i][1])
+        queueString += "Now playing: **%s**\n\n" % \
+                    (self.musicList[ctx.guild.id][0][1])
+        for i in range(1, len(self.musicList[ctx.guild.id])):
+            queueString += "%i.  **%s**\n" % \
+                    (i + 1, self.musicList[ctx.guild.id][i][1])
         await ctx.send(queueString)
 
     @commands.command()
@@ -166,21 +175,22 @@ class Music(Base):
         "Skips the video currently playing in the queue."
         if not await self.checkPermissions(ctx):
             return
-        elif not self.voice.is_playing():
+        elif not self.voice[ctx.guild.id].is_playing():
             await ctx.send("I'm not playing audio at the moment.")
         else:
-            await ctx.send("Skipping: **%s**" % (self.musicList[0][1]))
-            self.voice.stop()
+            await ctx.send("Skipping: **%s**" % \
+            (self.musicList[ctx.guild.id][0][1]))
+            self.voice[ctx.guild.id].stop()
 
     @commands.command()
     async def pause(self, ctx):
         "Pauses playback. Does nothing if not playing audio."
         if not await self.checkPermissions(ctx):
             return
-        elif not self.voice.is_playing():
+        elif not self.voice[ctx.guild.id].is_playing():
             await ctx.send("I'm not playing audio at the moment.")
         else:
-            self.voice.pause()
+            self.voice[ctx.guild.id].pause()
             await ctx.send("Pausing audio playback.")
 
     @commands.command()
@@ -188,10 +198,10 @@ class Music(Base):
         "Resumes audio playback, if paused. Does nothing otherwise."
         if not await self.checkPermissions(ctx):
             return
-        elif not self.voice.is_paused():
+        elif not self.voice[ctx.guild.id].is_paused():
             await ctx.send("Playback isn't paused!")
         else:
-            self.voice.resume()
+            self.voice[ctx.guild.id].resume()
             await ctx.send("Resuming audio playback.")
 
 def setup(bot):
